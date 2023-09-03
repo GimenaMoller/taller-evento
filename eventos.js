0416a3f0a79e0f66bf6cc1df6bb3243ffa400432
GimenaMoller <137663779+GimenaMoller@users.noreply.github.com>
2023-09-03 20:25:53 -0300

    commit 1

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.getElementById("saludarBtn").addEventListener("click", function() {
+    alert("Hola!");
+});
+
+document.getElementById("miDiv").addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
+
